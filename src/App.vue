b8445535dfc37a114236545d0eff03304de9f88f
allenhuang0713 <52902232+allenhuang0713@users.noreply.github.com>
2023-02-05 17:12:43 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="title">Hello Vue.js 3 World!</div>
+</template>
+<script setup lang="ts"></script>
+<style lang="scss" scoped>
+.title {
+  font-weight: bold;
+  font-size: 2rem;
+}
+</style>
